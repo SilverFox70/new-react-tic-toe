ff3c81bfba737fb8337142e2cbdcfd09922a9eae
Bill Brinkert <william.brinkert@akqa.com>
2018-12-07 16:06:58 -0800

    feat: basic tutorial complete; refactored to move components and utils into separate files

---
+++ src/Board.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import './index.css';
+import styled from '@emotion/styled/macro';
+import Square from './Square';
+import calculateWinner from './calculateWinner';
+
+const StatusDiv = styled.div`
+  margin-bottom: 10px;
+`;
+
+const BoardRow = styled.div`
+  :after {
+  clear: both;
+  content: "";
+  display: table;
+}
+`;
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    // Pass a function into setState because we are using current state
+    // to help determine future state for xIsNext
+    this.setState(
+      (prevState, props) => {
+        return ({
+          squares: squares,
+          xIsNext: !prevState.xIsNext
+        })
+      }, 
+        // Log out that state was updated as expected in the callback
+        () => {
+          console.log('squares: ', this.state.squares);
+        }
+      );
+  }
+
+  renderSquare(i) {
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={ () => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
+
+    return (
+      <div>
+        <StatusDiv>{status}</StatusDiv>
+        <BoardRow>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </BoardRow>
+        <BoardRow>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </BoardRow>
+        <BoardRow>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </BoardRow>
+      </div>
+    );
+  }
+}
+
+export default Board;
