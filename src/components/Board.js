95d85890da03dfcb6e6077738a7e4779ec6129cd
Bill Brinkert <william.brinkert@akqa.com>
2018-12-07 16:21:36 -0800

    chore: remove unused import

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import '../styles/index.css';
 import styled from '@emotion/styled/macro';
 import Square from './Square';
 import calculateWinner from './calculateWinner';
