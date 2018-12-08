90a7bd7b6a0f69adf294d64be503fb16333bd386
Bill Brinkert <william.brinkert@akqa.com>
2018-12-07 16:17:18 -0800

    feat: move components into directories; all component styling using @emotion

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import '../styles/index.css';
 import styled from '@emotion/styled/macro';
 import Square from './Square';
 import calculateWinner from './calculateWinner';
